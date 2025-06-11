c7a3c26269000163563907885c72452f7619c6f9
Ulisses João de Oliveira <ulissesjoao2005@gmail.com>
2025-06-10 22:35:43 -0300

    Remove build.sh and publish.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-RUSTFLAGS="-Zlocation-detail=none -Zfmt-debug=none" cargo +nightly build --release
-
-mv target/release/freem.exe .
-
-# shellcheck disable=SC2035
-ls -lh *.exe
